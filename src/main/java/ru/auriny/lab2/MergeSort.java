398d9c0b1b93bc57b12b2bf9f0495e7bfbbee947
Auriny <49393428+Auriny@users.noreply.github.com>
2026-09-12 00:19:56 +0600

    :sparkles: feat: merge sorting

---
+++ src/main/java/ru/auriny/lab2/MergeSort.java
@@ -0,0 +1,40 @@
+package ru.auriny.lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSort implements Sorting<Integer> {
+    private void slice(List<Integer> nums, int start, int end) {
+        var mid = start + (end - start) / 2;
+
+        slice(nums, start, mid); // левая половина
+        slice(nums, mid + 1, end); // правая половина
+
+        merge(nums, start, mid, end);
+    }
+
+    private void merge(List<Integer> nums, int start, int middle, int end) {
+        var leftPtr = start;
+        var rightPtr = middle + 1;
+
+        List<Integer> temp = new ArrayList<>();
+
+        while (leftPtr <= middle && rightPtr <= end) {
+            if (nums.get(leftPtr) <= nums.get(rightPtr)) {
+                temp.add(nums.get(leftPtr++));
+            } else temp.add(nums.get(rightPtr++));
+        }
+
+        while (leftPtr <= middle) temp.add(nums.get(leftPtr++));
+        while (rightPtr <= end) temp.add(nums.get(rightPtr++));
+
+        for (int i = 0; i < temp.size(); i++) {
+            nums.set(i, temp.get(i));
+        }
+    }
+
+    @Override
+    public void sort(List<Integer> nums) {
+        slice(nums, 0, nums.size() - 1);
+    }
+}
---
+++ src/test/java/ru/auriny/lab2/MergeSortTests.java
@@ -0,0 +1,18 @@
+package ru.auriny.lab2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSortTests {
+    @Test
+    void mergeSort() {
+        Sorting<Integer> sort = new MergeSort();
+        List<Integer> list = new ArrayList<>(List.of(1, 2, 5, 1, 1, 2, 6, 8, 9, 3));
+        sort.sort(list);
+
+        Assertions.assertEquals(new ArrayList<>(List.of(1, 1, 1, 2, 2, 3, 5, 6, 8, 9)), list);
+    }
+}
