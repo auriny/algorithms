572cf7d86b19f5f6fa7d4af3442c6d2c9baaa072
Auriny <49393428+Auriny@users.noreply.github.com>
2026-09-16 18:20:46 +0600

    :bug: fix: comment mistake

--- src/main/java/ru/auriny/lab1/ThirdSolution.java
+++ src/main/java/ru/auriny/lab1/ThirdSolution.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class ThirdSolution {
     public boolean containsDuplicate(List<Integer> nums) {
-        // это о(н), а за о(лог(н)) я хз как, сортировкой наверное
+        // это о(н), а за о(н лог(н)) я хз как, сортировкой наверное
         return nums.size() != new HashSet<>(nums).size();
     }
 }
